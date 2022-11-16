a83fb6cec1ee893730d209d384cbbebf1119b6ea
Octavio_paula00 <81445553+octaviopaula@users.noreply.github.com>
2022-11-16 13:49:27 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,16 @@
+window.audios = [
+  {
+    title:
+      "Agora sua radio Araguaia",
+    artist: "Araguaia Materiais para Construção",
+    cover: "Araguaia.jpg",
+    file: "Knockin' On Heaven's Door.mp3"
+  },
+  {
+    title:
+      "Agora sua radio Araguaia",
+    artist: "Araguaia Materiais para Construção",
+    cover: "Araguaia.jpg",
+    file: "Bon Jovi - You give love a bad name.mp3"
+  },
+];
